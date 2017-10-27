666e7f1a0c12e2fea17aa47eb276c78de5019de2
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-10-27 22:35:09 +0200

    Añadir TextWatcher a cada objeto EditText
    Mediante el uso del metodo addOnTextChangedListener, se le añade un
    oyente TextWatcher a cada objeto EditText.
    
    TextWatcher es disparado cuando el texto de un objeto EditText es
    modificado, mediante el metodo setText o entrada por teclado.
    
    El metodo afterTextChanged se ejecuta cuando el texto ya ha sido cambiado.
    
    Con esto, la calculadora se calcula instananeamente mientras se
    introduce el número.

--- CalculadoraSimple/app/src/main/java/org/institutoserpis/juanminm/calculadorasimple/MainActivity.java
+++ CalculadoraSimple/app/src/main/java/org/institutoserpis/juanminm/calculadorasimple/MainActivity.java
@@ -3,39 +3,88 @@ package org.institutoserpis.juanminm.calculadorasimple;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText num1EditText;
+    EditText num2EditText;
+    EditText resultadoEditText;
+    RadioGroup seleccionOperacionGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText num1EditText = (EditText) findViewById(R.id.activity_main_et_num1);
-        final EditText num2EditText = (EditText) findViewById(R.id.activity_main_et_num2);
-        final EditText resultadoEditText = (EditText) findViewById(R.id.activity_main_et_resultado);
-        final RadioGroup seleccionOperacionGroup = (RadioGroup) findViewById(
+        num1EditText = (EditText) findViewById(R.id.activity_main_et_num1);
+        num2EditText = (EditText) findViewById(R.id.activity_main_et_num2);
+        resultadoEditText = (EditText) findViewById(R.id.activity_main_et_resultado);
+        seleccionOperacionGroup = (RadioGroup) findViewById(
                 R.id.activity_main_rg_seleccionar_operacion);
 
-        seleccionOperacionGroup.clearCheck();
+        num1EditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (canCalculate())
+                    calculate();
+            }
+        });
+
+        num2EditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (canCalculate())
+                    calculate();
+            }
+        });
+
         seleccionOperacionGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
-                double resultado = 0;
-                double num1 = Double.parseDouble(num1EditText.getText().toString());
-                double num2 = Double.parseDouble(num2EditText.getText().toString());
-
-                if (group.getCheckedRadioButtonId() == R.id.activity_main_rb_suma) {
-                    resultado = num1 + num2;
-                }
-                if (group.getCheckedRadioButtonId() == R.id.activity_main_rb_resta) {
-                    resultado = num1 - num2;
-                }
-
-                resultadoEditText.setText(String.valueOf(resultado));
+                calculate();
             }
         });
+    }
+
+    private boolean canCalculate() {
+        return !num1EditText.getText().toString().equals("") &&
+                !num1EditText.getText().toString().equals("-") &&
+                !num1EditText.getText().toString().equals(".") &&
+                !num2EditText.getText().toString().equals("") &&
+                !num2EditText.getText().toString().equals("-") &&
+                !num2EditText.getText().toString().equals(".") ;
+    }
+
+    private void calculate() {
+        double resultado = 0;
+        double num1 = Double.parseDouble(num1EditText.getText().toString());
+        double num2 = Double.parseDouble(num2EditText.getText().toString());
+
+        if (seleccionOperacionGroup.getCheckedRadioButtonId() == R.id.activity_main_rb_suma) {
+            resultado = num1 + num2;
+        }
+        if (seleccionOperacionGroup.getCheckedRadioButtonId() == R.id.activity_main_rb_resta) {
+            resultado = num1 - num2;
+        }
+
+        resultadoEditText.setText(String.valueOf(resultado));
     }
 }
