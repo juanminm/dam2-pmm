faddac6ca511ff634676fe4ff836a70084a10b76
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2018-02-02 13:56:07 +0100

    Add Player class

---
+++ ProyectoFinal/app/src/main/java/org/institutoserpis/juanminm/proyectofinal/Player.java
@@ -0,0 +1,44 @@
+package org.institutoserpis.juanminm.proyectofinal;
+
+/**
+ * Created by juamar on 2/02/18.
+ */
+
+public class Player {
+    private long id;
+    private String name;
+    private double money;
+
+    public Player(){
+       this("", 0);
+    }
+
+    Player(String name, double money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+}
