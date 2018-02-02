bf275deea7d4ae3369bc1a9c2a61bfe230f2f969
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2018-02-02 13:38:53 +0100

    Use a generalized DatabaseHelper

---
+++ ProyectoFinal/app/src/main/java/org/institutoserpis/juanminm/proyectofinal/DatabaseHelper.java
@@ -0,0 +1,104 @@
+package org.institutoserpis.juanminm.proyectofinal;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by juamar on 19/01/18.
+ */
+
+public class DatabaseHelper {
+    private Context mCtx = null;
+    private DataBaseHelperInternal mDbHelper = null;
+    private SQLiteDatabase mDb = null;
+
+    public DatabaseHelper(Context ctx) {
+        this.mCtx = ctx;
+    }
+
+    private static class DataBaseHelperInternal extends SQLiteOpenHelper {
+        DataBaseHelperInternal(Context context, String databaseName, int databaseVersion) {
+            super(context, databaseName, null, databaseVersion);
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+            createTables(db);
+            fillTables(db);
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            deleteTables(db);
+            createTables(db);
+            fillTables(db);
+        }
+
+        private void createTables(SQLiteDatabase db) {
+            db.execSQL(Globals.CREATE_TABLE_PLAYER);
+            db.execSQL(Globals.CREATE_TABLE_ITEM);
+            db.execSQL(Globals.CREATE_TABLE_INVENTORY);
+            db.execSQL(Globals.CREATE_TABLE_MARKET);
+        }
+
+        private void deleteTables(SQLiteDatabase db) {
+            db.execSQL("DROP TABLE IF EXISTS " + Globals.TABLE_PLAYER_NAME);
+            db.execSQL("DROP TABLE IF EXISTS " + Globals.TABLE_ITEM_NAME);
+            db.execSQL("DROP TABLE IF EXISTS " + Globals.TABLE_INVENTORY_NAME);
+            db.execSQL("DROP TABLE IF EXISTS " + Globals.TABLE_MARKET_NAME);
+        }
+
+        private void fillTables(SQLiteDatabase db) {
+            //TODO
+//            db.execSQL(Globals.FILL_TABLE_PLAYER);
+//            db.execSQL(Globals.FILL_TABLE_ITEM);
+//            db.execSQL(Globals.FILL_TABLE_INVENTORY);
+//            db.execSQL(Globals.FILL_TABLE_MARKET);
+        }
+    }
+
+    public DatabaseHelper open() {
+        mDbHelper = new DataBaseHelperInternal(mCtx, Globals.DATABASE_NAME, Globals.DATABASE_VERSION);
+        mDb = mDbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        mDbHelper.close();
+    }
+
+    public Cursor getItems(String table, String[] columns, String selection, String[] selArgs, String orderBy) {
+        return mDb.query(
+                table,
+                columns,
+                selection,
+                selArgs,
+                null,
+                null,
+                orderBy);
+    }
+
+    public long insertItem(String table, String[][] data) {
+        ContentValues initialValues = new ContentValues();
+
+        for (String[] field : data)
+            initialValues.put(field[0], field[1]);
+
+        return mDb.insert(table, null, initialValues);
+    }
+
+    public int updateItem(String table, String where, String[] whereArgs, String[][] data) {
+        ContentValues contentValues = new ContentValues();
+
+        for (String[] field : data)
+            contentValues.put(field[0], field[1]);
+
+        return mDb.update(table, contentValues, where, whereArgs);
+    }
+
+    public int delete(String table, String where, String[] whereArgs) {
+        return mDb.delete(table, where, whereArgs);
+    }
+
+}
