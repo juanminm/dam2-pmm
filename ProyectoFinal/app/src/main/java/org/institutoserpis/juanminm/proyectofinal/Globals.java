c8662b74961f7de7424513bede90ff417c089fc6
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2018-02-14 12:51:21 +0100

    Change database filename and reset version

--- ProyectoFinal/app/src/main/java/org/institutoserpis/juanminm/proyectofinal/Globals.java
+++ ProyectoFinal/app/src/main/java/org/institutoserpis/juanminm/proyectofinal/Globals.java
@@ -7,8 +7,8 @@ package org.institutoserpis.juanminm.proyectofinal;
 
 public final class Globals {
     // Database settings
-    public static final String DATABASE_NAME = "GameMarket";
-    public static final int DATABASE_VERSION = 2;
+    public static final String DATABASE_NAME = "GameMarket.db";
+    public static final int DATABASE_VERSION = 1;
 
     // Table Player
     public static final String TABLE_PLAYER_NAME = "player";
